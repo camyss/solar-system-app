fdb31c2ca9b7e44577e0bdd16c2861cb32930af2
Camily <camily.rocha07@gmail.com>
2023-07-31 18:24:25 -0300

    fix: update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculo from './Components/calculo';
+import DetalhesCalculo from './Components/detalhesCalculo';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/calculo" element={<Calculo />} />
+        <Route path="/calculo/:id" element={<DetalhesCalculo />} />
       </Routes>
     </Router>
   );
